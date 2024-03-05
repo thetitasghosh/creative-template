4f178bad297f07eaace948d7988d78039496403b
Titas Ghosh <rg814602@gmail.com>
2024-03-05 20:56:17 +0530

    creative development tamplate

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { cn}  from '@/library/utils/cn'
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className={cn('text-8xl text-white')}>Creative Template</h1>
+    </main>
+  )
+}
