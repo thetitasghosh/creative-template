dfa70c8c1b5335a9a5cba47aac62f4fff51b9f07
Titas Ghosh <rg814602@gmail.com>
2024-03-05 21:03:38 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { cn}  from '@/library/utils/cn'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className={cn('text-8xl text-white')}>Creative Template</h1>
+      <h1 className={cn('text-8xl text-white')}>Creative Template Titas</h1>
     </main>
   )
 }
