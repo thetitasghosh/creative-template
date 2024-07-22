3925131a49222a0743165cf725dceb0487771569
Titas Ghosh <rg814602@gmail.com>
2024-07-22 20:27:26 +0530

    tailwind screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,14 @@ const config = {
   ],
   prefix: "",
   theme: {
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     container: {
       center: true,
       padding: "2rem",
